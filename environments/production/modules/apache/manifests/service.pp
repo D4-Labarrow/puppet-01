aa2b9d2cde71622dc821d47b82386716ccf3d47b
danis <david@labarrow.fr>
2021-01-27 22:46:01 +0100

    servie update

--- environments/production/modules/apache/manifests/service.pp
+++ environments/production/modules/apache/manifests/service.pp
@@ -1,7 +1,8 @@
-class apache::service
+class apache::service {
   service { "${apache::service_name}":
-    ensure     => $apache:service_ensure,
-    enable     => $apache:service_enable,
+    alias      => 'apache_service',
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
     hasrestart => true,
   }
  }
